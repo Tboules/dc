6c6b8a85ec0a0f6132fbc6e289b9da4222b797c8
Tboules <tboules@gmail.com>
2024-02-23 10:08:21 -0800

    drizzle next auth adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import db from "@/lib/database";
+import { Adapter } from "next-auth/adapters";
 
-export const handler = NextAuth({
+const nextAuthConfig = {
+  adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
+} satisfies AuthOptions;
+
+export const handler = NextAuth(nextAuthConfig);
 
 export { handler as GET, handler as POST };
