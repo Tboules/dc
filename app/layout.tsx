c44543b2c7ad2fea076d5d5e2e6d5e772218db63
Tboules <tboules@gmail.com>
2024-02-26 15:08:53 -0800

    Nav Styles 20%

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Poppins as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { getServerSession } from "next-auth";
@@ -7,7 +7,12 @@ import NavMenu from "@/components/nav/NavMenu";
 import SessionProvider from "@/components/providers/session-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
-const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
+const fontSans = FontSans({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Desert Collections",
--- components/nav/NavMenu.tsx
+++ components/nav/NavMenu.tsx
@@ -2,6 +2,8 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { Menu } from "lucide-react";
+import { Separator } from "../ui/separator";
 
 function AuthButton() {
   const { data: session } = useSession();
@@ -15,22 +17,26 @@ function AuthButton() {
 
 export default function NavMenu() {
   return (
-    <nav className="w-full">
-      <div className="p-4 bg-secondary text-secondary-foreground w-full max-w-screen-xl m-auto flex justify-between items-center">
-        <Link href="/">
+    <nav className="sticky top-0 z-50 bg-background/80 backdrop-blur border-b border-b-border/60">
+      <div className="p-4 text-secondary-foreground w-full max-w-screen-xl m-auto flex items-center gap-4">
+        <Link href="/" className="flex-1">
           <h1 className="text-lg font-semibold">Desert Collections</h1>
         </Link>
-        <div className="flex items-center flex-1 justify-center gap-4">
+        <div className="hidden sm:flex items-center justify-center gap-8">
           <Link href="/desert_figures">Desert Figures</Link>
-          <Link href="/tags">tags</Link>
-          <Link href="/icons">icons</Link>
+          <Link href="/tags">Tags</Link>
+          <Link href="/icons">Icons</Link>
         </div>
-        <div className="flex items-center gap-4">
+        <Separator className="h-4" orientation="vertical" />
+        <div className="hidden sm:flex items-center gap-4">
           <Link href="/excerpt/new">
             <Button variant={"outline"}>Add</Button>
           </Link>
           <AuthButton />
         </div>
+        <Button className="sm:hidden" size="icon" variant={"ghost"}>
+          <Menu />
+        </Button>
       </div>
     </nav>
   );
