7710d0c38cd4f554170d6f201b00f4ce3db112b9
Tboules <tboules@gmail.com>
2024-03-13 16:34:09 -0700

    form: add author 10%

--- app/excerpt/new/page.tsx
+++ app/excerpt/new/page.tsx
@@ -1,5 +1,23 @@
+import {
+  DESERT_FIGURE_TYPE,
+  NewDesertFigure,
+  newDesertFigureSchema,
+} from "@/lib/database/schema/desertFigures";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+
+/*
+  fields for ui:
+  1. first name
+  2. last name
+  3. title (Desert Figure Title)
+  4. epithet 
+  5. type
+  6. thumbnail
+*/
 
 export default async function NewExcerptFormPage() {
   const session = await getServerSession();
@@ -9,9 +27,29 @@ export default async function NewExcerptFormPage() {
     );
   }
 
+  // can't use hooks in server comp
+  // const form = useForm<NewDesertFigure>({
+  //   resolver: zodResolver(newDesertFigureSchema),
+  //   defaultValues: {
+  //     title: "",
+  //     firstName: "",
+  //     lastName: "",
+  //     epithet: "",
+  //     thumbnail: "",
+  //     type: DESERT_FIGURE_TYPE.AUTHOR,
+  //   },
+  // });
+  //
+  // function onSubmit(values: NewDesertFigure) {
+  //   console.log(values);
+  // }
+
   return (
-    <div>
-      <h1>New Excerpt Form Page restricted</h1>
+    <div className="max-w-screen-xl m-auto bg-slate-800 p-4">
+      <h1>hello add form</h1>
     </div>
   );
 }
+// <Form {...form}>
+//   <form onSubmit={form.handleSubmit(onSubmit)}></form>
+// </Form>
--- lib/database/schema/desertFigures.ts
+++ lib/database/schema/desertFigures.ts
@@ -2,11 +2,23 @@ import { pgTable, text, timestamp, uuid, smallint } from "drizzle-orm/pg-core";
 import { users } from "./users";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
+import { ObjectValues } from "@/lib/utils";
 
-export enum DesertFigureType {
-  SUBJECT = 2,
-  AUTHOR = 1,
-}
+export const DESERT_FIGURE_TYPE = {
+  AUTHOR: 1,
+  SUBJECT: 2,
+} as const;
+
+export type DesertFigureType = ObjectValues<typeof DESERT_FIGURE_TYPE>;
+
+export const DESERT_FIGURE_TITLE = {
+  SAINT: "Saint",
+  DOCTOR: "Doctor",
+  FATHER: "Father",
+  MOTHER: "Mother",
+} as const;
+
+export type DesertFigureTitle = ObjectValues<typeof DESERT_FIGURE_TITLE>;
 
 export const desertFigures = pgTable("desert_figure", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -21,8 +33,8 @@ export const desertFigures = pgTable("desert_figure", {
   createdBy: text("added_by").references(() => users.id),
 });
 
-export const newDesertFigureSchema = createInsertSchema(desertFigures);
 export const desertFigureSchema = createSelectSchema(desertFigures);
+export const newDesertFigureSchema = createInsertSchema(desertFigures);
 
 export type DesertFigure = z.infer<typeof desertFigureSchema>;
 export type NewDesertFigure = z.infer<typeof newDesertFigureSchema>;
