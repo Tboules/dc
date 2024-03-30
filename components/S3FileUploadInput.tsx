1c5266ed69dc99872ba4675a1adfa81a5ecc88e8
Tboules <tboules@gmail.com>
2024-03-29 17:18:25 -0700

    s3 upload comp 90%

--- components/S3FileUploadInput.tsx
+++ components/S3FileUploadInput.tsx
@@ -1,7 +1,10 @@
-import { ChangeEvent } from "react";
+import React, { ChangeEvent } from "react";
 import { Input, InputProps } from "./ui/input";
 import { Label } from "./ui/label";
 import { v4 as uuidv4 } from "uuid";
+import { ObjectValues } from "@/lib/enums";
+import Lottie from "react-lottie-player";
+import lottieAnimation from "@/assets/loading.json";
 
 interface Props extends InputProps {
   setFinalImageUrl: (id: string) => void;
@@ -11,17 +14,31 @@ interface Props extends InputProps {
   1. generate file path uuid -- 
   2. get presigned --
   3. upload file --
-  4. save file url with uuid within form
-  5. create cool loading animation
+  4. save file url with uuid within form -- 
+  5. create cool loading animation -- 
   6. image preview
 */
+
+const S3_FILE_UPLOAD_STATUS = {
+  init: "init",
+  loading: "loading",
+  error: "error",
+  success: "success",
+} as const;
+
+type S3fileUploadStatus = ObjectValues<typeof S3_FILE_UPLOAD_STATUS>;
 
 export default function S3FileUploadInput({
   setFinalImageUrl,
   ...rest
 }: Props) {
+  const [status, setStatus] = React.useState<S3fileUploadStatus>(
+    S3_FILE_UPLOAD_STATUS.init,
+  );
+
   async function uploadFile(e: ChangeEvent<HTMLInputElement>) {
     if (!e.target.files) return;
+    setStatus("loading");
     let fileId = uuidv4();
     const fileUrl = process.env.NEXT_PUBLIC_AWS_S3_URL + fileId;
 
@@ -40,19 +57,38 @@ export default function S3FileUploadInput({
 
       if (!fileUploadResponse.ok) {
         console.log("file upload error");
+        setStatus("error");
         return;
       }
 
       setFinalImageUrl(fileUrl);
+      setStatus("success");
     } catch (error) {
       console.log(error);
+      setStatus("error");
     }
   }
 
   return (
     <div className="space-y-2">
       <Label>Thumbnail</Label>
-      <Input {...rest} type="file" accept="image/*" onChange={uploadFile} />
+      {status == "loading" && (
+        <div className="h-10 border border-border rounded relative overflow-clip">
+          <Lottie
+            className="absolute bottom-0 -left-12 -right-12"
+            loop
+            animationData={lottieAnimation}
+            play
+          />
+        </div>
+      )}
+      <Input
+        {...rest}
+        type="file"
+        accept="image/*"
+        className={status == "loading" ? "hidden" : ""}
+        onChange={uploadFile}
+      />
     </div>
   );
 }
