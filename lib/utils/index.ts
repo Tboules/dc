c9a97675b99117f80fb82c57e41f774c683a1690
Tboules <tboules@gmail.com>
2024-03-30 12:07:30 -0700

    regex uuid utils

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,6 +1,15 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { uuidv4Regex } from "./regex";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function pullUuidFromUrl(url: string) {
+  const match = url.match(uuidv4Regex);
+
+  if (match) {
+    return match[0];
+  }
 }
