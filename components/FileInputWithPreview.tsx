3a28f101fd45ab7a2bd325cdf93552235a2ed5ea
Tboules <tboules@gmail.com>
2024-04-01 12:04:35 -0700

    typescript issues

--- components/FileInputWithPreview.tsx
+++ components/FileInputWithPreview.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Input, InputProps } from "./ui/input";
-import { Image } from "lucide-react";
+import { Image as ImageIcon } from "lucide-react";
 
 interface Props extends InputProps {}
 
@@ -10,12 +10,14 @@ const FileInputWithPreview = React.forwardRef<HTMLInputElement, Props>(
       <div className="space-y-2">
         <Input className="" ref={ref} {...rest} type="file" accept="image/*" />
         <div className=" border border-border rounded h-72 flex flex-col justify-center items-center">
-          <Image strokeWidth={1} height={64} width={64} />
+          <ImageIcon strokeWidth={1} height={64} width={64} />
           <h4 className="font-medium text-lg">Image Preview</h4>
         </div>
       </div>
     );
   },
 );
+
+FileInputWithPreview.displayName = "FileInputWithPreview";
 
 export default FileInputWithPreview;
