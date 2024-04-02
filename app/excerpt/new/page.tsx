703012f44d2422cfca8590b3bf6748ca3a7b986a
Tboules <tboules@gmail.com>
2024-04-02 11:30:55 -0700

    combo box research

--- app/excerpt/new/page.tsx
+++ app/excerpt/new/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import PostFigureForm from "@/components/forms/post-figure";
+import DesertFigureSelect from "@/components/desert-figure-select";
 
 export default async function NewExcerptFormPage() {
   const session = await getServerSession();
@@ -12,7 +13,8 @@ export default async function NewExcerptFormPage() {
 
   return (
     <div className="max-w-screen-xl flex flex-col items-center m-auto p-4">
-      <PostFigureForm />
+      <DesertFigureSelect />
+      {/*<PostFigureForm /> */}
     </div>
   );
 }
