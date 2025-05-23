a9f409c29c23a5f020323f3f76af6ef6f26b4f63
Tboules <tboules@gmail.com>
2025-05-23 12:52:04 -0700

    truncate and fix max screen issue

--- components/safe-html-renderer.tsx
+++ components/safe-html-renderer.tsx
@@ -1,9 +1,18 @@
+const HTML_STRING_MAX_LENGTH = 300;
+
 export default function SafeHtmlRenderer({
   htmlString,
 }: {
   htmlString: string;
 }) {
-  const markup = { __html: htmlString };
+  const truncatedString =
+    htmlString.length > HTML_STRING_MAX_LENGTH
+      ? htmlString.substring(0, HTML_STRING_MAX_LENGTH) + "..."
+      : htmlString;
+
+  const markup = {
+    __html: truncatedString,
+  };
 
   return <div className="text-lg" dangerouslySetInnerHTML={markup} />;
 }
