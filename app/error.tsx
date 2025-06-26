a80937c9a9b0673b6af5435f4bc82f89d33067d0
Tboules <tboules@gmail.com>
2025-06-26 15:49:52 -0700

    app level error boundary for now

---
+++ app/error.tsx
@@ -0,0 +1,24 @@
+"use client"; // Error boundaries must be Client Components
+
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="max-w-screen-lg mx-auto p-8 flex flex-col justify-center items-center">
+      <h2 className="text-2xl mb-8 text-center">Something went wrong!</h2>
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  );
+}
