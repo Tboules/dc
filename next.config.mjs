7f3baabdd0803d9a4b2b3d01586d4ac7e138ba3e
Tboules <tboules@gmail.com>
2025-12-18 14:11:06 -0800

    turbo pack

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const withRoutes = nextRoutes();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   reactStrictMode: true,
   crossOrigin: "anonymous",
   images: {
